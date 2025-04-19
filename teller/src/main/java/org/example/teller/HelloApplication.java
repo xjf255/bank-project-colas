5022a4bb485bee0f2a2a836ff01dc86eca3a402f
favasquezUMG <fvasquezl6@miumg.edu.gt>
2025-04-19 17:15:55 -0600

    Se realizo todo lo necesario para poder hacer el apartado grafico

--- teller/src/main/java/org/example/teller/HelloApplication.java
+++ teller/src/main/java/org/example/teller/HelloApplication.java
@@ -3,18 +3,30 @@ package org.example.teller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import utilities.Paths;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        /*
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("servicioCaja.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 558.0, 475.0);
+        Scene scene1 = new Scene(fxmlLoader.load());
+        stage.setTitle("Servicio Caja");
+        stage.setScene(scene1);
+        stage.show();
+        //*/
+        //*
+        AnchorPane load = FXMLLoader.load(getClass().getResource(Paths.SERVICIO_CAJA));
+        Scene scene = new Scene(load);
+        stage.setTitle("Servicio Caja");
         stage.setScene(scene);
         stage.show();
+        //*/
     }
 
     public static void main(String[] args) {
