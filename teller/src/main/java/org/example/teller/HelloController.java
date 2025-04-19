0195a09a5f73910de175b56930f5b1f04feccc29
favasquezUMG <fvasquezl6@miumg.edu.gt>
2025-04-19 17:15:55 -0600

    Se realizo todo lo necesario para poder hacer el apartado grafico

--- teller/src/main/java/org/example/teller/HelloController.java
+++ teller/src/main/java/org/example/teller/HelloController.java
@@ -1,14 +1,31 @@
 package org.example.teller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button bttnNextTurn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button bttnRealizar;
+
+    @FXML
+    private TextField txtfMontoAcreditar;
+
+    @FXML
+    private TextField txtfNumCuenta;
+
+    @FXML
+    void realizarDeposito(ActionEvent event) {
+        txtfNumCuenta.setText("Funciona el boton de deposito");
     }
+
+    @FXML
+    void siguienteTurno(ActionEvent event) {
+        txtfMontoAcreditar.setText("Funciona el boton de siguiente turno");
+    }
+
 }
---
+++ teller/src/main/java/utilities/Paths.java
@@ -0,0 +1,5 @@
+package utilities;
+
+public class Paths {
+    public static final String SERVICIO_CAJA = "/servicioCaja.fxml";
+}
